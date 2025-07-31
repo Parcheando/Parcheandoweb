608a1e4d4c875e83ac394e5d45e74630dd77288f
Parcheando <parcheandoweb@gmail.com>
2025-07-31 13:11:07 +0200

    Create models.py

---
+++ models.py
@@ -0,0 +1,8 @@
+class Comic:
+    def __init__(self, titulo, empresa, heroe, anio, numero, descripcion):
+        self.titulo = titulo
+        self.empresa = empresa
+        self.heroe = heroe
+        self.anio = anio
+        self.numero = numero
+        self.descripcion = descripcion
